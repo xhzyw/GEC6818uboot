7c82455257b59231d6ccb20b911a798c194da5d7
pkc <2224947977@qq.com>
2024-10-12 19:30:11 +0800

    🌈 style:修改uboot用户名

--- include/configs/GEC6818.h
+++ include/configs/GEC6818.h
@@ -138,7 +138,7 @@
 /*-----------------------------------------------------------------------
  * Miscellaneous configurable options
  */
-#define CONFIG_SYS_PROMPT				"pkc# "     									/* Monitor Command Prompt   */
+#define CONFIG_SYS_PROMPT				"x6818# "     									/* Monitor Command Prompt   */
 #define CONFIG_SYS_LONGHELP				       												/* undef to save memory	   */
 #define CONFIG_SYS_CBSIZE		   		800		   											/* Console I/O Buffer Size  */
 #define CONFIG_SYS_PBSIZE		   		(CONFIG_SYS_CBSIZE+sizeof(CONFIG_SYS_PROMPT)+16) 	/* Print Buffer Size */
